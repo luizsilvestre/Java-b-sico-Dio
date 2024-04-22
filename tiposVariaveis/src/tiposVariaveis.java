7c755690fd100bd93bbe2a2dcede313db9a5e868
Luiz Silvestre Schwartz <120575172+luizsilvestre@users.noreply.github.com>
2024-04-22 02:27:19 -0300

    Atualizar o tiposVariaveis.java

--- tiposVariaveis/src/tiposVariaveis.java
+++ tiposVariaveis/src/tiposVariaveis.java
@@ -1,5 +1,9 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
+int cartazes =1;
+double salario =2740;
+
+
         System.out.println("Hello, World!");
     }
 }
